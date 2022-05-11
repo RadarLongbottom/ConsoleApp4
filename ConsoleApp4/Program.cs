37786b0d4e7be8e62c1e5df3fd54f19fcd8cbb02
Aleksander 60726 <gdx151579@student.wsb.gda.pl>
2022-05-11 10:05:24 +0200

    1

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,26 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Witaj w aplikacji przelicznika walut!!!");
+﻿Console.WriteLine("Witaj w aplikacji przelicznika walut!!!");
+Console.WriteLine();
+Console.WriteLine("Naciśnij dowolny przycisk aby rozpocząć...");
+Console.ReadKey(true);
+Console.WriteLine();
 Console.WriteLine("Wybierz walutę bazową: ");
 Console.WriteLine("1 - Złoty");
 Console.WriteLine("2 - Dolar Amerykański");
 Console.WriteLine("3 - Euro");
 Console.WriteLine("4 - Funt Brytyjski");
 Console.Write("Twój wybór waluty bazowej: ");
-var walB = Console.ReadLine();
+double walB = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine();
 switch (walB)
 {
-    case "1":
+    case 1:
         Console.WriteLine("Wybrano walutę: Polski Złoty");
         break;
-    case "2":
+    case 2:
         Console.WriteLine("Wybrano walutę: Dolar Amerykański");
-        break ;
-    case "3":
+        break;
+    case 3:
         Console.WriteLine("Wybrano walutę: Euro");
         break;
-    case "4":
+    case 4:
         Console.WriteLine("Wybrano walutę: Funt Brytyjski");
         break;
 }
@@ -31,67 +33,186 @@ Console.WriteLine("2 - Dolar Amerykański");
 Console.WriteLine("3 - Euro");
 Console.WriteLine("4 - Funt Brytyjski");
 Console.Write("Twój wybór waluty, na którą chcesz przeliczyć: ");
-var walP = Console.ReadLine();
+double walP = Convert.ToDouble(Console.ReadLine());
 switch (walP)
 {
-    case "1":
+    case 1:
         Console.WriteLine("Wybrano walutę: Polski Złoty");
         break;
-    case "2":
+    case 2:
         Console.WriteLine("Wybrano walutę: Dolar Amerykański");
         break;
-    case "3":
+    case 3:
         Console.WriteLine("Wybrano walutę: Euro");
         break;
-    case "4":
+    case 4:
         Console.WriteLine("Wybrano walutę: Funt Brytyjski");
         break;
 }
 Console.WriteLine();
-if (walB == "1")
+Console.Write("Podaj kwotę do przeliczenia: ");
+double kwota = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine();
+double wynik = 0;
+//double przelicznikZD = 4.44;
+//double PrzelicznikZE = 4.68;
+//double PrzelicznikZF = 5.47;
+if (walB == 1)
 {
-    Convert.ToString(walB);
-    walB = "Polski Złoty";
+    if (walP == 2)
+    {
+        wynik = kwota / 4.45;
+    }
+    else if (walP == 3)
+    {
+        wynik = kwota / 4.7;
+    }
+    else if (walP == 4)
+    {
+        wynik = kwota / 5.5;
+    }
 }
-else if (walB == "2")
+if (walB == 2)
 {
-    Convert.ToString(walB);
-    walB = "Dolar Amerykański";
+    if (walP == 1)
+    {
+        wynik = kwota * 4.45;
+    }
+    else if (walP == 3)
+    {
+        wynik = kwota * 0.95;
+    }
+    else if (walP == 4)
+    {
+        wynik = kwota * 0.80;
+    }
 }
-else if (walB == "3")
+if (walB == 3)
 {
-    Convert.ToString(walB);
-    walB = "Euro";
+    if (walP == 1)
+    {
+        wynik = kwota * 4.7;
+    }
+    else if (walP == 2)
+    {
+        wynik = kwota * 1.05;
+    }
+    else if (walP == 4)
+    {
+        wynik = kwota * 0.85;
+    }
 }
-else if (walB == "4")
+if (walB == 4)
 {
-    Convert.ToString(walB);
-    walB = "Funt Brytyjski";
-}
-if (walP == "1")
-{
-    Convert.ToString(walP);
-    walP = "Polski Złoty";
-}
-else if (walP == "2")
-{
-    Convert.ToString(walP);
-    walP = "Dolar Amerykański";
-}
-else if (walP == "3")
-{
-    Convert.ToString(walP);
-    walP = "Euro";
-}
-else if (walP == "4")
-{
-    Convert.ToString(walP);
-    walP = "Funt Brytyjski";
+    if (walP == 1)
+    {
+        wynik = kwota * 5.45;
+    }
+    else if (walP == 2)
+    {
+        wynik = kwota * 1.2;
+    }
+    else if (walP == 3)
+    {
+        wynik = kwota * 1.15;
+    }
 }
 
-Console.WriteLine("Przeliczamy walutę: {0} na walutę: {1}", walB, walP);
-Console.WriteLine();
-Console.Write("Podaj kwotę jaką chcesz przeliczyć: ");
-double kwota = 15;
-Console.WriteLine("{0} {1} to {3}", kwota, walB, walP);
-Console.ReadKey(); 
+if (walB == 1 && walP == 2)
+{
+    string w = "PLN";
+    string z = walB.ToString(w);
+    string x = "USD";
+    string y = walP.ToString(x);
+    Console.WriteLine("Po przeliczeniu {0} {1} na {2} wyszło {3} {4}", kwota, z, y, wynik, y);
+}
+else if (walB == 1 && walP == 3)
+{
+    string w = "PLN";
+    string z = walB.ToString(w);
+    string x = "EUR";
+    string y = walP.ToString(x);
+    Console.WriteLine("Po przeliczeniu {0} {1} na {2} wyszło {3} {4}", kwota, z, y, wynik, y);
+}
+else if (walB == 1 && walP == 4)
+{
+    string w = "PLN";
+    string z = walB.ToString(w);
+    string x = "GBP";
+    string y = walP.ToString(x);
+    Console.WriteLine("Po przeliczeniu {0} {1} na {2} wyszło {3} {4}", kwota, z, y, wynik, y);
+}
+else if (walB == 2 && walP == 1)
+{
+    string w = "USD";
+    string z = walB.ToString(w);
+    string x = "PLN";
+    string y = walP.ToString(x);
+    Console.WriteLine("Po przeliczeniu {0} {1} na {2} wyszło {3} {4}", kwota, z, y, wynik, y);
+}
+else if (walB == 2 && walP == 3)
+{
+    string w = "USD";
+    string z = walB.ToString(w);
+    string x = "EUR";
+    string y = walP.ToString(x);
+    Console.WriteLine("Po przeliczeniu {0} {1} na {2} wyszło {3} {4}", kwota, z, y, wynik, y);
+}
+else if (walB == 2 && walP == 4)
+{
+    string w = "USD";
+    string z = walB.ToString(w);
+    string x = "GBP";
+    string y = walP.ToString(x);
+    Console.WriteLine("Po przeliczeniu {0} {1} na {2} wyszło {3} {4}", kwota, z, y, wynik, y);
+}
+else if (walB == 3 && walP == 1)
+{
+    string w = "EUR";
+    string z = walB.ToString(w);
+    string x = "PLN";
+    string y = walP.ToString(x);
+    Console.WriteLine("Po przeliczeniu {0} {1} na {2} wyszło {3} {4}", kwota, z, y, wynik, y);
+}
+else if (walB == 3 && walP == 2)
+{
+    string w = "EUR";
+    string z = walB.ToString(w);
+    string x = "USD";
+    string y = walP.ToString(x);
+    Console.WriteLine("Po przeliczeniu {0} {1} na {2} wyszło {3} {4}", kwota, z, y, wynik, y);
+}
+else if (walB == 3 && walP == 4)
+{
+    string w = "EUR";
+    string z = walB.ToString(w);
+    string x = "GBP";
+    string y = walP.ToString(x);
+    Console.WriteLine("Po przeliczeniu {0} {1} na {2} wyszło {3} {4}", kwota, z, y, wynik, y);
+}
+else if (walB == 4 && walP == 1)
+{
+    string w = "GBP";
+    string z = walB.ToString(w);
+    string x = "PLN";
+    string y = walP.ToString(x);
+    Console.WriteLine("Po przeliczeniu {0} {1} na {2} wyszło {3} {4}", kwota, z, y, wynik, y);
+}
+else if (walB == 4 && walP == 2)
+{
+    string w = "GBP";
+    string z = walB.ToString(w);
+    string x = "USD";
+    string y = walP.ToString(x);
+    Console.WriteLine("Po przeliczeniu {0} {1} na {2} wyszło {3} {4}", kwota, z, y, wynik, y);
+}
+else if (walB == 4 && walP == 3)
+{
+    string w = "GBP";
+    string z = walB.ToString(w);
+    string x = "EUR";
+    string y = walP.ToString(x);
+    Console.WriteLine("Po przeliczeniu {0} {1} na {2} wyszło {3} {4}", kwota, z, y, wynik, y);
+}
+
+Console.ReadKey();
